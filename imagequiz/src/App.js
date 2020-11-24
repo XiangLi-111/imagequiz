c5eace68f481d475e19f5bbbdf566819b1c2c053
XiangLi-111 <xiangl2@email.arizona.edu>
2020-11-24 16:35:04 +1100

    add switch

--- imagequiz/src/App.js
+++ imagequiz/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 
 import LogInPage from "./pages/loginpage";
 import HomePage from "./pages/homepage";
